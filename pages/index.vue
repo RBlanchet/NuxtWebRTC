2471b32e26a50c1989ab869821cc30414e970b0b
BLANCHET Romain <contact@blanchet-romain.fr>
2020-03-23 15:18:03 +0100

    Initialisation du Socket côté serveur et utilisation du getUserMedia

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="container">
-    <h1>NuxtWebRTC</h1>
+    <h1 class="text-center">NuxtWebRTC</h1>
+    <div class="rounded overflow-hidden shadow-lg flex justify-center">
+      <video autoplay ref="localVideo"></video>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    navigator.getUserMedia(
+      { video: true, audio: true },
+      stream => {
+        const localVideo = this.$refs['localVideo'];
+        if (localVideo) {
+          localVideo.srcObject = stream;
+        }
+      },
+      error => {
+        console.warn(error.message);
+      }
+    );
+  }
 }
 </script>
--- server/index.js
+++ server/index.js
@@ -1,33 +1,45 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const express = require('express');
+const consola = require('consola');
+const { Nuxt, Builder } = require('nuxt');
+const app = express();
+const {createServer} = require('http');
+const socketIo = require('socket.io');
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+const config = require('../nuxt.config.js');
+config.dev = process.env.NODE_ENV !== 'production';
 
 async function start () {
   // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server
+  const { host, port } = nuxt.options.server;
 
-  await nuxt.ready()
+  await nuxt.ready();
   // Build only in dev mode
   if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+    const builder = new Builder(nuxt);
+    await builder.build();
   }
 
   // Give nuxt middleware to express
-  app.use(nuxt.render)
+  app.use(nuxt.render);
+
+  const httpServer = createServer(app);
+  const io = socketIo(httpServer);
+
+  io.on('connection', socket => {
+    console.log('Socket connecté');
+  });
 
   // Listen the server
-  app.listen(port, host)
+  httpServer.listen(port, host);
+
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+    badge: true,
+  });
 }
-start()
+
+start();
+
